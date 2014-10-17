1733a62c2d9806376b359f5ff7528218ddfd8f28
Shamray Alexander <im.samuray@gmail.com>
2014-10-17 19:54:51 +0300

    add more tests

--- true_false.php
+++ true_false.php
@@ -1,7 +1,37 @@
 <?php
-$x = (!(strtolower('no') === 'no'));
-if ($x) {
-  echo '$x should be false!';
-  var_dump($x);
+$false = (!(strtolower('No') === 'no'));
+$same = (strtolower('No') === 'no');
+$notSame = (strtolower('No') !== 'no');
+$equal = (strtolower('No') == 'no');
+$notEqual = (strtolower('No') != 'no');
+
+if ($false) {
+  echo '$false should be false!';
+  var_dump($false);
+}
+
+if (!$same) {
+  echo '$same should be true!';
+  var_dump($same);
+}
+
+if ($notSame) {
+  echo '$notSame should be false!';
+  var_dump($notSame);
+}
+
+if (!$equal) {
+  echo '$equal should be true!';
+  var_dump($equal);
+}
+
+if ($notEqual) {
+  echo '$notEqual should be false!';
+  var_dump($notEqual);
+}
+
+if (!$false && $same && !$notSame && $equal && !$notEqual) {
+  exit(0);
+} else {
   exit(1);
 }
